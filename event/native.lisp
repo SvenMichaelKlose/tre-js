7ef337549528884923b78bb4cd1541ff6ad85664
Sven Michael Klose <pixel@copei.de>
2019-01-28 17:01:19 +0100

    Removed IE support.

--- event/native.lisp
+++ event/native.lisp
@@ -1,14 +1,11 @@
+; DO NOT USE THESE!
+; TODO: Remove all of these since Internet Explorer is gone. (Yes!)
+
 (fn native-add-event-listener (elm typ fun)
-  (with (f [funcall fun _])
-    (?
-      elm.add-event-listener (elm.add-event-listener typ f false)
-      elm.attach-event       (elm.attach-event (+ "on" typ) f)
-      (error "Can't determine function to add the event listener for '~A' on DOM object ~A." typ elm))))
+  (elm.add-event-listener typ [funcall fun _] false))
 
 (fn native-remove-event-listener (elm typ fun)
-  (? elm.remove-event-listener
-	 (elm.remove-event-listener typ fun false)
-	 (elm.detach-event (+ "on" typ) fun)))
+   (elm.remove-event-listener typ fun false))
 
 (fn native-stop-event (evt)
   (& evt.prevent-default
@@ -17,11 +14,9 @@
      (evt.stop-propagation)))
 
 (fn native-fire-event (elm event-name)
-  (? document.create-event-object
-     (elm.fire-event (+ "on" event-name) (document.create-event-object))
-     (let evt (document.create-event "HTMLEvents")
-       (evt.init-event event-name t t)
-       (not (elm.dispatch-event evt)))))
+  (!= (document.create-event "HTMLEvents")
+    (!.init-event event-name t t)
+    (elm.dispatch-event !)))
 
 (fn native-fire-d-o-m-content-loaded (&optional (html-document document))
   (native-fire-event document "DOMContentLoaded"))
