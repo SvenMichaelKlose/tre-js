2ac167d36a1b66e1422dc81bdf8e037b96a2d73f
Sven Michael Klose <pixel@copei.de>
2019-01-28 17:01:08 +0100

    Use VAR instead of DEFVAR.

--- unicode.lisp
+++ unicode.lisp
@@ -1,57 +1,57 @@
-(defvar *copyright-symbol* 169)
-(defvar *copyright-symbol-string* (string (code-char *copyright-symbol*)))
-(defvar *registered-symbol* 174)
-(defvar *degree-symbol* 176)
-(defvar *times* 215)
-(defvar *en-space* 8194)
-(defvar *em-space* 8195)
-(defvar *em3-space* 8196)
-(defvar *em4-space* 8197)
-(defvar *em6-space* 8198)
-(defvar *figure-space* 8199)
-(defvar *punctuation-space* 8200)
-(defvar *thin-space* 8201)
-(defvar *hair-space* 8202)
-(defvar *lsquote* 8216)     ; single quote, bottom
-(defvar *sbquote* 8218)     ; single quote, top
-(defvar *ldquote* 8228)     ; double quote, bottom
-(defvar *bdquote* 8230)     ; double quote, top
-(defvar *ellipsis* 8230)
-(defvar *ellipsis-string* (string (code-char 8230)))
-(defvar *solidus* 8260)     ; fraction
+(var *copyright-symbol* 169)
+(var *copyright-symbol-string* (string (code-char *copyright-symbol*)))
+(var *registered-symbol* 174)
+(var *degree-symbol* 176)
+(var *times* 215)
+(var *en-space* 8194)
+(var *em-space* 8195)
+(var *em3-space* 8196)
+(var *em4-space* 8197)
+(var *em6-space* 8198)
+(var *figure-space* 8199)
+(var *punctuation-space* 8200)
+(var *thin-space* 8201)
+(var *hair-space* 8202)
+(var *lsquote* 8216)     ; single quote, bottom
+(var *sbquote* 8218)     ; single quote, top
+(var *ldquote* 8228)     ; double quote, bottom
+(var *bdquote* 8230)     ; double quote, top
+(var *ellipsis* 8230)
+(var *ellipsis-string* (string (code-char 8230)))
+(var *solidus* 8260)     ; fraction
 
-(defvar *hyphen-minus* 45)  ; Generic use
-(defvar *hyphen* 8208)  ; Compound words
-(defvar *hyphen-string* (string (code-char *hyphen*)))
-(defvar *non-breaking-hyphen* 8209)  ; Compound words
-(defvar *en-dash* 8211)  ; Ranges
-(defvar *en-dash-string* (string (code-char *en-dash*)))  ; Ranges
-(defvar *em-dash* 8212)  ; Break of thought
-(defvar *em-dash-string* (string (code-char *em-dash*)))
-(defvar *minus-sign* 8722)  ; Break of thought
+(var *hyphen-minus* 45)  ; Generic use
+(var *hyphen* 8208)  ; Compound words
+(var *hyphen-string* (string (code-char *hyphen*)))
+(var *non-breaking-hyphen* 8209)  ; Compound words
+(var *en-dash* 8211)  ; Ranges
+(var *en-dash-string* (string (code-char *en-dash*)))  ; Ranges
+(var *em-dash* 8212)  ; Break of thought
+(var *em-dash-string* (string (code-char *em-dash*)))
+(var *minus-sign* 8722)  ; Break of thought
 
-(defvar *degree-celsius* 8451)
-(defvar *degree-fahrenheit* 8457)
-(defvar *trademark-symbol* 8482)
-(defvar *kelvin-sign* 8490)
+(var *degree-celsius* 8451)
+(var *degree-fahrenheit* 8457)
+(var *trademark-symbol* 8482)
+(var *kelvin-sign* 8490)
 
-(defvar *arrow-left* 8592)
-(defvar *arrow-left-string* (string (code-char 8592)))
-(defvar *arrow-up* 8593)
-(defvar *arrow-up-string* (string (code-char 8593)))
-(defvar *arrow-right* 8594)
-(defvar *arrow-right-string* (string (code-char 8594)))
-(defvar *arrow-down* 8595)
-(defvar *arrow-down-string* (string (code-char 8595)))
+(var *arrow-left* 8592)
+(var *arrow-left-string* (string (code-char 8592)))
+(var *arrow-up* 8593)
+(var *arrow-up-string* (string (code-char 8593)))
+(var *arrow-right* 8594)
+(var *arrow-right-string* (string (code-char 8594)))
+(var *arrow-down* 8595)
+(var *arrow-down-string* (string (code-char 8595)))
 
-(defvar *tab-key* 8677)
-(defvar *tab-key-string* (string (code-char 8677)))
+(var *tab-key* 8677)
+(var *tab-key-string* (string (code-char 8677)))
 
-(defvar *shift-key* 8679)
-(defvar *shift-key-string* (string (code-char 8679)))
+(var *shift-key* 8679)
+(var *shift-key-string* (string (code-char 8679)))
 
-(defvar *backspace-key* 9003)
-(defvar *backspace-key-string* (string (code-char 9003)))
+(var *backspace-key* 9003)
+(var *backspace-key-string* (string (code-char 9003)))
 
-(defvar *return-key* 9166)
-(defvar *return-key-string* (string (code-char 9166)))
+(var *return-key* 9166)
+(var *return-key-string* (string (code-char 9166)))
