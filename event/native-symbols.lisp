45b21a291b746b5404f5417c10a90b4d4f3fa97d
Sven Michael Klose <pixel@copei.de>
2019-01-29 16:48:57 +0100

    Removed IE symbols.

--- event/native-symbols.lisp
+++ event/native-symbols.lisp
@@ -1,10 +1,8 @@
 (dont-obfuscate
     client-x client-y
     add-event-listener
-    attach-event
     dispatch-event
     remove-event-listener
-    detach-event
     prevent-default
     stop-propagation
     type button char-code key-code target
