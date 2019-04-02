38ea1d348c792469b54de3c97cb068c7f258e65d
Sven Michael Klose <pixel@hugbox.org>
2019-04-02 19:18:02 +0200

    tré does not support obfuscation anymore.

--- event/native-symbols.lisp
+++
@@ -1,9 +0,0 @@
-(dont-obfuscate
-    client-x client-y
-    add-event-listener
-    dispatch-event
-    remove-event-listener
-    prevent-default
-    stop-propagation
-    type button char-code key-code target
-    cancel-bubble return-value)
