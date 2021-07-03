b3cc64518faf6c9d5e7f7cba7bd2f044257971e6
Sven Michael Klose <pixel@hugbox.org>
2021-07-03 15:13:01 +0200

    New macro UNBIND-EVENT-METHODS.

--- event/bind-event-methods.lisp
+++ event/bind-event-methods.lisp
@@ -7,6 +7,13 @@
 (defmacro bind-event-methods-element (elm &rest event-types)
   `(progn
      ,@(@ [`((slot-value ,elm 'add-event-listener) ,(downcase (symbol-name _))
-                                                   (bind (slot-value this ',($ '_ _)))
+                                                   (= (slot-value this ',($ '_ _)) (bind (slot-value this ',($ '_ _))))
                                                    ,elm)]
+          event-types)))
+
+(defmacro unbind-event-methods (elm &rest event-types)
+  `(progn
+     ,@(@ [`((slot-value ,elm 'remove-event-listener) ,(downcase (symbol-name _))
+                                                      (slot-value this ',($ '_ _))
+                                                      ,elm)]
           event-types)))
