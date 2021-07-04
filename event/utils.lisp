51ce817841a2bb48838f8791dca20c9ab52cd919
Sven Michael Klose <pixel@hugbox.org>
2021-07-04 13:10:35 +0200

    File split.

--- event/utils.lisp
+++ event/utils.lisp
@@ -25,33 +25,3 @@
 
 (fn event-right-button? e
   (!= e.button 1))
-
-(var *key-stats* (make-array))
-(var *last-click-shift-down?* nil)
-(var *shift-down?* nil)
-(var *ctrl-down?* nil)
-(var *alt-down?* nil)
-
-(fn get-key-stat (code)
-  (unless (undefined? (aref *key-stats* code))
-    (aref *key-stats* code)))
-
-(fn update-keystat (evt)
-  (= (aref *key-stats* evt.key-code) (eql evt.type "keydown"))
-  (= *shift-down?* (get-key-stat 16)
-     *ctrl-down?*  (get-key-stat 17)
-     *alt-down?*   (get-key-stat 18)))
-
-(document.keyup update-keystat)
-(document.keydown update-keystat)
-
-(var *pointer-x* 0)
-(var *pointer-y* 0)
-
-(document.mousemove [(= *pointer-x* _.page-x
-                        *pointer-y* _.page-y)])
-
-(document.click [(log-message "trclick")(= *last-click-shift-down?* (log-message *shift-down?*)]))
-
-(var *last-hovered* nil)
-(document.mouseover [= *last-hovered* _.target])
