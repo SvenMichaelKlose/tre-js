cc85393fa2b97ccaed3f98a0e667e3e60e6bc82c
Sven Michael Klose <pixel@hugbox.org>
2021-07-04 14:32:29 +0200

    LOG-MESSAGE renamed to DUMP.

--- dump.lisp
+++ dump.lisp
@@ -1,13 +1,15 @@
 (fn make-log-stream ()
   (make-stream :fun-in   []
                :fun-out  #'((c str)
-                             (log-message (? (string? c) c (char-string c))))
+                             (dump (? (string? c) c (char-string c))))
                :fun-eof  [identity t]))
 
 (var *standard-log* (make-log-stream))
 (= *standard-output* (make-log-stream))
 (= *standard-error* (make-log-stream))
 
-(fn log-message (x)
+(fn dump (x &optional (title nil))
+  (& title
+     (console.log (+ title ":")))
   (console.log x)
   x)
