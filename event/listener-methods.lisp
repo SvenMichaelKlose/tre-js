6e563e691ccd9e46db137def6038dfccea642b3a
Sven Michael Klose <pixel@hugbox.org>
2021-07-04 16:13:08 +0200

    Clean up macro names.

--- event/listener-methods.lisp
+++ event/listener-methods.lisp
@@ -1,17 +1,11 @@
-(defmacro bind-event-methods (event-module &rest event-types)
-  `(progn
-     ,@(@ [`((car this._elements).add-event-listener ,(downcase (symbol-name _))
-                                                     (bind (slot-value this ',($ '_ _))))]
-          event-types)))
-
-(defmacro bind-event-methods-element (elm &rest event-types)
+(defmacro add-listener-methods (elm &rest event-types)
   `(progn
      ,@(@ [`((slot-value ,elm 'add-event-listener) ,(downcase (symbol-name _))
                                                    (= (slot-value this ',($ '_ _)) (bind (slot-value this ',($ '_ _))))
                                                    ,elm)]
           event-types)))
 
-(defmacro unbind-event-methods (elm &rest event-types)
+(defmacro remove-listener-methods (elm &rest event-types)
   `(progn
      ,@(@ [`((slot-value ,elm 'remove-event-listener) ,(downcase (symbol-name _))
                                                       (slot-value this ',($ '_ _))
